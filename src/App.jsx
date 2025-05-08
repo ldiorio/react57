1b6bc6def05336f94b11a21bd93243f4b40e93fe
ldiorio <lisa.diorio@gmail.com>
2025-05-07 20:34:09 -0400

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       
-      <h1>React Working Online!</h1>
+      <h1>React is Working Online!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
