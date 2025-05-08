f492073f5854b233fb13e7747f5e24968ad8803a
ldiorio <lisa.diorio@gmail.com>
2025-05-07 20:31:56 -0400

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(100)
+
+  return (
+    <>
+      
+      <h1>React Working Online!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
